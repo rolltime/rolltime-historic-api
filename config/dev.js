f522fb4dc75a279313fe31bcac65f0c43b2c2c1d
Luis Capelo <luiscape@gmail.com>
2015-09-11 23:14:33 -0400

    bumping to 0.1.1

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'version': 'v.0.1.0',
+  'version': 'v.0.1.1',
   'description': 'Service to query the historic public bike-sharing data.',
   'repository': 'https://github.com/rolltime/rolltime-historic-api',
   'maintainer': 'Luis Capelo <luiscape@gmail.com>'
